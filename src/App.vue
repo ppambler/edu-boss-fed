9b39b1dcfe1e05959a25ce5758732952379ffd7e
ppambler <allinlbj@gmail.com>
2021-08-01 00:25:15 +0800

    样式处理

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@
     <h1>拉钩教育</h1>
     <!-- 根路由出口 -->
     <router-view />
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+    <el-button>normal button</el-button>
   </div>
 </template>
 
