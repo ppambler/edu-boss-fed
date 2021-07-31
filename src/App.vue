a40c9a27d5f30b4efa311a1127f9a8304bf3f8e9
ppambler <allinlbj@gmail.com>
2021-07-31 19:46:47 +0800

    导入 Element 组件库

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,24 @@
     <!-- 用来提示还有内容，不然一片空白，以为项目有问题 -->
     <h1>拉钩教育</h1>
     <!-- 根路由出口 -->
-    <router-view/>
+    <router-view />
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'App'
+})
+</script>
 
+<style lang="scss" scoped>
 </style>
