49161afdda3ac9f1a68e1f55fe33d6a1cfcc6194
ppambler <allinlbj@gmail.com>
2021-08-02 19:24:30 +0800

    布局-初始化路由组件

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,16 @@
     <h1>拉钩教育</h1>
     <!-- 根路由出口 -->
     <router-view />
-    <el-button>normal button</el-button>
-    <p>yellow</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import request from '@/utils/request'
-request({
-  method: 'GET',
-  url: '/boss/v2/api-docs?group=edu-boss-boot'
-}).then(data => {
-  console.log(data)
-})
 export default Vue.extend({
   name: 'App'
 })
 </script>
 
 <style lang="scss" scoped>
-p
-{
-    font: $font-kai;
-
-    color: $warning-color;
-}
 
 </style>
