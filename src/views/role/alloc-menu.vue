c6cb7d5bd051a2ff2bbe1e15db0fcef87b160fd5
ppambler <allinlbj@gmail.com>
2021-09-11 19:00:23 +0800

    分配菜单-以树形结构展示所有菜单列表

--- src/views/role/alloc-menu.vue
+++ src/views/role/alloc-menu.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="alloc-menu">分配菜单</div>
+  <div class="alloc-menu">
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      show-checkbox
+      default-expand-all
+    ></el-tree>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { getMenuNodeList } from '@/services/menu'
+
 export default Vue.extend({
   name: 'AllocMenu',
   props: {
     roleId: {
       type: [String, Number],
       required: true
+    }
+  },
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'subMenuList',
+        label: 'name'
+      }
+    }
+  },
+  created () {
+    this.loadMenus()
+  },
+  methods: {
+    async loadMenus () {
+      const { data } = await getMenuNodeList()
+      this.menus = data.data
     }
   }
 })
