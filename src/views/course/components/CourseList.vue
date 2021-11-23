cd029a04492a471cf0ecb14f433ec3de074db33b
ppambler <allinlbj@gmail.com>
2021-11-23 13:34:25 +0800

    课程管理-处理课程上下架

--- src/views/course/components/CourseList.vue
+++ src/views/course/components/CourseList.vue
@@ -41,14 +41,30 @@
         v-loading="loading"
         style="width: 100%; margin-bottom: 20px"
       >
-        <el-table-column prop="id" label="ID" width="50" align="center"> </el-table-column>
+        <el-table-column prop="id" label="ID" width="50" align="center">
+        </el-table-column>
         <el-table-column prop="courseName" label="课程名称" align="center">
         </el-table-column>
-        <el-table-column prop="price" label="价格" align="center" :formatter="formatPrice"> </el-table-column>
-        <el-table-column prop="sortNum" label="排序" align="center"> </el-table-column>
+        <el-table-column
+          prop="price"
+          label="价格"
+          align="center"
+          :formatter="formatPrice"
+        >
+        </el-table-column>
+        <el-table-column prop="sortNum" label="排序" align="center">
+        </el-table-column>
         <el-table-column prop="status" label="上架状态" align="center">
           <template slot-scope="scope">
-            <el-switch v-model="scope.row.status" active-color="#13ce66" inactive-color="#ff4949" :active-value="1" :inactive-value="0">
+            <el-switch
+              v-model="scope.row.status"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              :active-value="1"
+              :inactive-value="0"
+              :disabled="scope.row.isStatusLoading"
+              @change="onStateChange(scope.row)"
+            >
             </el-switch>
           </template>
         </el-table-column>
@@ -81,7 +97,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { getQueryCourses } from '@/services/course'
+import { getQueryCourses, changeState } from '@/services/course'
 import { Form } from 'element-ui'
 
 export default Vue.extend({
@@ -108,6 +124,9 @@ export default Vue.extend({
     async loadCourses () {
       this.loading = true
       const { data } = await getQueryCourses(this.filterParams)
+      data.data.records.forEach((item: any) => {
+        item.isStatusLoading = false
+      })
       this.courses = data.data.records
       this.totalCount = data.data.total
       this.loading = false
@@ -140,6 +159,17 @@ export default Vue.extend({
 
     formatPrice (item: any) {
       return `￥${item.price}`
+    },
+
+    async onStateChange (course: any) {
+      // console.log(course)
+      course.isStatusLoading = true
+      const { data } = await changeState({
+        courseId: course.id,
+        status: course.status
+      })
+      this.$message.success(`${course.status === 0 ? '下架' : '上架'}成功`)
+      course.isStatusLoading = false
     }
   }
 })
