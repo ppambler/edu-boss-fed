d19abc854975ee6d1ee9fb8f523d2a0b8afdcd8f
ppambler <allinlbj@gmail.com>
2021-11-26 18:07:31 +0800

    课程内容-上传课时视频-处理完成

--- src/views/course/video.vue
+++ src/views/course/video.vue
@@ -19,6 +19,12 @@
           <el-button type="primary" @click="handleUpload">开始上传</el-button>
           <el-button @click="$router.back()">返回</el-button>
         </el-form-item>
+        <el-form-item>
+          <p>视频上传中：{{ uploadPercent }}%</p>
+          <p v-if="isUploadSucess">
+            视频转码中：{{ isTransCodeSuccess ? "完成" : "正在处理，请稍后" }}
+          </p>
+        </el-form-item>
       </el-form>
     </el-card>
   </div>
@@ -28,14 +34,20 @@
 import Vue from 'vue'
 import {
   getAliyunImagUploadAddressAdnAuth,
-  getAliyunVideoUploadAddressAdnAuth
+  getAliyunVideoUploadAddressAdnAuth,
+  aliyunTransCode,
+  getAliyunTransCodePercent
 } from '@/services/aliyun-upload'
 export default Vue.extend({
   name: 'CourseVideo',
   data () {
     return {
       uploader: null,
-      imageURL: ''
+      imageURL: '',
+      videoId: null,
+      uploadPercent: 0,
+      isUploadSucess: false,
+      isTransCodeSuccess: false
     }
   },
   computed: {
@@ -82,63 +94,96 @@ export default Vue.extend({
               imageUrl: this.imageURL // 请确保一定是先上传图片
             })
             uploadAddressAndAuth = data.data
+            this.videoId = uploadAddressAndAuth.videoId
           }
 
           // 2. 调用 uploader.setUploadAuthAndAddress 设置上传凭证
           /* eslint-disable */
-          ;(this.uploader as any).setUploadAuthAndAddress(
+          (this.uploader as any).setUploadAuthAndAddress(
             uploadInfo,
             uploadAddressAndAuth.uploadAuth,
             uploadAddressAndAuth.uploadAddress,
             uploadAddressAndAuth.imageId || uploadAddressAndAuth.videoId
-          )
+          );
           // 3. 设置好上传凭证确认没有问题，上传进度开始
 
           // setUploadAuthAndAddress(uploadFileInfo, uploadAuth, uploadAddress,videoId);
         },
         // 文件上传成功
         onUploadSucceed: function (uploadInfo: any) {
-          console.log('onUploadSucceed', uploadInfo)
+          console.log("onUploadSucceed", uploadInfo);
         },
         // 文件上传失败
         onUploadFailed: function (uploadInfo: any, code: any, message: any) {
-          console.log('onUploadFailed', uploadInfo, code, message)
+          console.log("onUploadFailed", uploadInfo, code, message);
         },
         // 文件上传进度，单位：字节
-        onUploadProgress: function (
+        onUploadProgress: (
           uploadInfo: any,
           totalSize: any,
           loadedPercent: any
-        ) {
-          console.log('onUploadProgress', uploadInfo, totalSize, loadedPercent)
+        ) => {
+          console.log("onUploadProgress", uploadInfo, totalSize, loadedPercent);
+          console.log("loadedPercent", loadedPercent);
+          if (!uploadInfo.isImage) {
+            this.uploadPercent = Math.floor(loadedPercent * 100);
+          }
         },
         // 上传凭证超时
         onUploadTokenExpired: function (uploadInfo: any) {
-          console.log('onUploadTokenExpired', 'uploadInfo')
+          console.log("onUploadTokenExpired", "uploadInfo");
         },
         // 全部文件上传结束
-        onUploadEnd: function (uploadInfo: any) {
-          console.log('onUploadEnd', uploadInfo)
-        }
-      })
+        onUploadEnd: async (uploadInfo: any) => {
+          console.log("onUploadEnd", uploadInfo);
+          this.isUploadSucess = true;
+          // 请求转码
+          const { data } = await aliyunTransCode({
+            lessonId: this.$route.query.lessonId,
+            coverImageUrl: this.imageURL,
+            fileName: (this.video as any).files[0].name,
+            fileId: this.videoId,
+          });
+
+          console.log(data);
+
+          // 轮询查询转码进度
+          const timer = setInterval(async () => {
+            const { data } = await getAliyunTransCodePercent(
+              this.$route.query.lessonId
+            );
+            console.log(data.data);
+            if (data.data === 100) {
+              this.isTransCodeSuccess = true;
+              window.clearInterval(timer);
+              console.log("转码成功");
+            }
+          }, 3000);
+        },
+      });
     },
 
-    handleUpload () {
+    handleUpload() {
+      // 初始化上传状态
+      this.isUploadSucess = false;
+      this.isTransCodeSuccess = false;
+      this.uploadPercent = 0;
+
       // 获取上传的文件
-      const videoFile = (this.video as any).files[0]
-      const imageFile = (this.image as any).files[0]
-      const uploader = this.uploader as any
+      const videoFile = (this.video as any).files[0];
+      const imageFile = (this.image as any).files[0];
+      const uploader = this.uploader as any;
 
       // 将用户所选的文件添加到上传列表中
       // 一旦开始上传，它就会按照列表中添加的顺序开始上传
-      uploader.addFile(imageFile, null, null, null, '{"Vod":{}}')
-      uploader.addFile(videoFile, null, null, null, '{"Vod":{}}')
+      uploader.addFile(imageFile, null, null, null, '{"Vod":{}}');
+      uploader.addFile(videoFile, null, null, null, '{"Vod":{}}');
 
       // 开始上传，触发 onUploadstarted 事件
-      uploader.startUpload()
-    }
-  }
-})
+      uploader.startUpload();
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped></style>
