a6ec8db99bf6c0e126183242dc678cce5bc30aaf
ppambler <allinlbj@gmail.com>
2021-11-26 23:35:54 +0800

    课程内容-富文本编辑器图片上传

--- src/components/TextEditor/index.vue
+++ src/components/TextEditor/index.vue
@@ -5,6 +5,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import E from 'wangeditor'
+import { uploadCourseImage } from '@/services/course'
 
 export default Vue.extend({
   name: 'TextEditor',
@@ -14,6 +15,21 @@ export default Vue.extend({
       default: ''
     }
   },
+  data () {
+    return {
+      editor: null,
+      hasValue: false
+    }
+  },
+  watch: {
+    value () {
+      if (!this.hasValue) {
+        /* eslint-disable */
+        ;(this.editor as any).txt.html(this.value)
+        this.hasValue = true
+      }
+    }
+  },
   // 组件已经渲染好，可以初始化操作 DOM 了
   mounted () {
     this.initEditor()
@@ -21,6 +37,7 @@ export default Vue.extend({
   methods: {
     initEditor () {
       const editor = new E(this.$refs.editor as any)
+      this.editor = editor as any
       // 注意：事件监听必须在 create 之前
       editor.config.onchange = (value: string) => {
         this.$emit('input', value)
@@ -28,7 +45,17 @@ export default Vue.extend({
       editor.create()
 
       // 注意：设置初始化必须在 create 之后
-      editor.txt.html(this.value)
+      // editor.txt.html(this.value)
+
+      editor.config.customUploadImg = async function (resultFiles: any, insertImgFn: any) {
+        // 1. 把用户选择的 resultFiles 上传到服务端
+        const fd = new FormData()
+        fd.append('file', resultFiles[0])
+        const { data } = await uploadCourseImage(fd)
+
+        // 2. 根据图片地址生成 img 标签插入富文本编辑器内容中
+        insertImgFn(data.data.name)
+      }
     }
   }
 })
