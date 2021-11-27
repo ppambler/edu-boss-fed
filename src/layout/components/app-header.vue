4f5bddc0b3f7db9a8c9d2ff82c4ad704cdb645b6
ppambler <allinlbj@gmail.com>
2021-11-27 16:42:35 +0800

    feat: auto breadcrumb

--- src/layout/components/app-header.vue
+++ src/layout/components/app-header.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="header">
-    <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>课程管理</el-breadcrumb-item>
-      <!-- <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item> -->
-    </el-breadcrumb>
+    <Breadcrumb />
     <el-dropdown>
       <span class="el-dropdown-link">
         <el-avatar
@@ -28,8 +23,12 @@
 <script lang="ts">
 import Vue from 'vue'
 import { getUserInfo } from '@/services/user'
+import Breadcrumb from '@/components/Breadcrumb/index.vue'
 export default Vue.extend({
   name: 'AppHeader',
+  components: {
+    Breadcrumb
+  },
   data () {
     return {
       userInfo: {} // 当前登录用户信息
@@ -48,23 +47,27 @@ export default Vue.extend({
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      }).then(() => { // 确认执行这里
-        // 清除登录状态
-        this.$store.commit('setUser', null)
-        // 跳转到登录页面
-        this.$router.push({
-          name: 'login'
-        })
-        this.$message({
-          type: 'success',
-          message: '退出成功！'
-        })
-      }).catch(() => { // 取消执行这里
-        this.$message({
-          type: 'info',
-          message: '已取消退出'
-        })
       })
+        .then(() => {
+          // 确认执行这里
+          // 清除登录状态
+          this.$store.commit('setUser', null)
+          // 跳转到登录页面
+          this.$router.push({
+            name: 'login'
+          })
+          this.$message({
+            type: 'success',
+            message: '退出成功！'
+          })
+        })
+        .catch(() => {
+          // 取消执行这里
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          })
+        })
     }
   }
 })
